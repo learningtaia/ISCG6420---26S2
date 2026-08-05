4724d1fd09bc983e808d5f8398b3ecdb360f1c34
learningtaia <172948223+learningtaia@users.noreply.github.com>
2026-08-05 20:16:18 +1200

    Create week 3 content for inclass activity and add exercise 4 files and complete exercise 4.

---
+++ week3/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    let myID = document.getElementById("myID");
+    myID.innerHTML = "Updated!";
+}
+
+window.addEventListener("DOMContentLoaded", myFunction);
+
+window.alert("yoooo");
+
